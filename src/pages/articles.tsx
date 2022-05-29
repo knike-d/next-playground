589ce1922e5708c01d237bb6c35e26cd88eec860
knike-d <knike.develop@gmail.com>
2022-05-29 23:47:46 +0900

    動的ルーティング

--- src/pages/articles.tsx
+++ src/pages/articles.tsx
@@ -1,7 +1,13 @@
 import type { NextPage } from "next";
+import Link from "next/link";
 import Head from "next/head";
+import { useRouter } from "next/router";
+
+const articleList = [...Array(2).keys()];
 
 const Articles: NextPage = () => {
+  const router = useRouter();
+
   return (
     <div>
       <Head>
@@ -9,6 +15,18 @@ const Articles: NextPage = () => {
       </Head>
       <main>
         <h1>記事一覧</h1>
+        <ul>
+          {articleList.map((el, index) => {
+            const articleNumber = index + 1;
+            return (
+              <li>
+                <Link href={`${router.pathname}/${articleNumber}`}>
+                  <a>記事{articleNumber}</a>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
       </main>
     </div>
   );
