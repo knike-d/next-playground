afededdf44a47026ccae59717efdc315a67a309c
knike-d <knike.develop@gmail.com>
2022-06-22 02:15:47 +0900

    日付ライブラリの導入とラッパー関数の作成

---
+++ src/utils/date.ts
@@ -0,0 +1,23 @@
+import { format, parseISO, compareAsc } from "date-fns";
+import { utcToZonedTime } from "date-fns-tz";
+
+export const DateFormat = {
+  YearMonthDate: "yyyy-MM-dd",
+};
+type DateFormat = typeof DateFormat[keyof typeof DateFormat];
+
+export const toUtc = (date: string): Date => parseISO(date);
+
+export const toJst = (date: string | Date): Date => {
+  const utcDate = typeof date === "string" ? toUtc(date) : date;
+  return utcToZonedTime(utcDate, "Asia/Tokyo");
+};
+
+export const jstToStr = (date: Date, dateFormat: DateFormat): string => format(date, dateFormat);
+
+export const utcToStr = (date: string | Date, dateFormat: DateFormat): string => {
+  const utcDate = typeof date === "string" ? toUtc(date) : date;
+  return jstToStr(toJst(utcDate), dateFormat);
+};
+
+export const sortDateByAsc = (a: number | Date, b: number | Date) => compareAsc(a, b);
